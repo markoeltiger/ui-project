79762f02b4a3e3cb0a04d610eb21ef3a1838d7f5
markoeltiger <43094629+markoeltiger@users.noreply.github.com>
2020-12-28 04:06:44 +0200

    Add files via upload

---
+++ Econtact.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace $safeprojectname$
+{
+    partial class Econtact
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Econtact));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CustomerId = new System.Windows.Forms.Label();
+            this.txtBoxContactId = new System.Windows.Forms.TextBox();
+            this.txtboxfirstname = new System.Windows.Forms.TextBox();
+            this.lblfirstname = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbllastname = new System.Windows.Forms.Label();
+            this.lblproblem = new System.Windows.Forms.Label();
+            this.lbltypeofproblem = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(258, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(287, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // CustomerId
+            // 
+            this.CustomerId.AutoSize = true;
+            this.CustomerId.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerId.Location = new System.Drawing.Point(39, 178);
+            this.CustomerId.Name = "CustomerId";
+            this.CustomerId.Size = new System.Drawing.Size(112, 22);
+            this.CustomerId.TabIndex = 1;
+            this.CustomerId.Text = "CustomerId";
+            // 
+            // txtBoxContactId
+            // 
+            this.txtBoxContactId.Location = new System.Drawing.Point(172, 178);
+            this.txtBoxContactId.Name = "txtBoxContactId";
+            this.txtBoxContactId.Size = new System.Drawing.Size(154, 22);
+            this.txtBoxContactId.TabIndex = 2;
+            this.txtBoxContactId.TextChanged += new System.EventHandler(this.txtBoxContactId_TextChanged);
+            // 
+            // txtboxfirstname
+            // 
+            this.txtboxfirstname.Location = new System.Drawing.Point(172, 240);
+            this.txtboxfirstname.Name = "txtboxfirstname";
+            this.txtboxfirstname.Size = new System.Drawing.Size(154, 22);
+            this.txtboxfirstname.TabIndex = 4;
+            // 
+            // lblfirstname
+            // 
+            this.lblfirstname.AutoSize = true;
+            this.lblfirstname.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfirstname.Location = new System.Drawing.Point(39, 238);
+            this.lblfirstname.Name = "lblfirstname";
+            this.lblfirstname.Size = new System.Drawing.Size(110, 22);
+            this.lblfirstname.TabIndex = 3;
+            this.lblfirstname.Text = "First Name";
+            this.lblfirstname.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(172, 303);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // lbllastname
+            // 
+            this.lbllastname.AutoSize = true;
+            this.lbllastname.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllastname.Location = new System.Drawing.Point(39, 303);
+            this.lbllastname.Name = "lbllastname";
+            this.lbllastname.Size = new System.Drawing.Size(72, 22);
+            this.lbllastname.TabIndex = 5;
+            this.lbllastname.Text = "Addrss";
+            // 
+            // lblproblem
+            // 
+            this.lblproblem.AutoSize = true;
+            this.lblproblem.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblproblem.Location = new System.Drawing.Point(39, 423);
+            this.lblproblem.Name = "lblproblem";
+            this.lblproblem.Size = new System.Drawing.Size(85, 22);
+            this.lblproblem.TabIndex = 7;
+            this.lblproblem.Text = "Problem";
+            // 
+            // lbltypeofproblem
+            // 
+            this.lbltypeofproblem.AutoSize = true;
+            this.lbltypeofproblem.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltypeofproblem.Location = new System.Drawing.Point(39, 364);
+            this.lbltypeofproblem.Name = "lbltypeofproblem";
+            this.lbltypeofproblem.Size = new System.Drawing.Size(134, 22);
+            this.lbltypeofproblem.TabIndex = 9;
+            this.lbltypeofproblem.Text = "Problem Type";
+            this.lbltypeofproblem.Click += new System.EventHandler(this.lbltypeofproblem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(172, 425);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(338, 96);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Land Line Mobile",
+            "Land Line Internet",
+            "Wifi",
+            "Mobile Phone"});
+            this.comboBox1.Location = new System.Drawing.Point(172, 366);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(154, 24);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSubmit.Font = new System.Drawing.Font("Rakkas", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(356, 551);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(208, 82);
+            this.btnSubmit.TabIndex = 12;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnUpdate.Font = new System.Drawing.Font("Rakkas", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(118, 654);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(190, 82);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Font = new System.Drawing.Font("Rakkas", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(364, 654);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(190, 82);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Blue;
+            this.btnClear.Font = new System.Drawing.Font("Rakkas", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(608, 654);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(190, 82);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(529, 232);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(357, 289);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(658, 178);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(228, 22);
+            this.txtSearch.TabIndex = 18;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearch.Location = new System.Drawing.Point(525, 178);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(71, 22);
+            this.lblSearch.TabIndex = 17;
+            this.lblSearch.Text = "Search";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Image = ((System.Drawing.Image)(resources.GetObject("CloseButton.Image")));
+            this.CloseButton.Location = new System.Drawing.Point(849, 0);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(66, 57);
+            this.CloseButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CloseButton.TabIndex = 19;
+            this.CloseButton.TabStop = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // Econtact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(912, 761);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lbltypeofproblem);
+            this.Controls.Add(this.lblproblem);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lbllastname);
+            this.Controls.Add(this.txtboxfirstname);
+            this.Controls.Add(this.lblfirstname);
+            this.Controls.Add(this.txtBoxContactId);
+            this.Controls.Add(this.CustomerId);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Econtact";
+            this.Text = "EContact";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label CustomerId;
+        private System.Windows.Forms.TextBox txtBoxContactId;
+        private System.Windows.Forms.TextBox txtboxfirstname;
+        private System.Windows.Forms.Label lblfirstname;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbllastname;
+        private System.Windows.Forms.Label lblproblem;
+        private System.Windows.Forms.Label lbltypeofproblem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.PictureBox CloseButton;
+    }
+}
+
---
+++ Econtact.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+namespace $safeprojectname$
+{
+    public partial class Econtact : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=MR-FSOCIETY;Initial Catalog=MarkoDatabase;Integrated Security=True");
+
+        public Econtact()
+        {
+            InitializeComponent();
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String Idphone = txtBoxContactId.Text;
+            String Name = txtboxfirstname.Text;
+            String adress = textBox2.Text;
+            String Problem = richTextBox1.Text;
+            SqlDataAdapter da = new SqlDataAdapter();
+            String apos = ",";
+        
+            SqlCommand cmd = new SqlCommand("INSERT INTO [dbo].[Table] (CustomerIDPhone,DName,Address) VALUES('Idphone' , 'MarkSiaf' ,'{adress}')",connection);
+            connection.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            connection.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lbltypeofproblem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBoxContactId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
